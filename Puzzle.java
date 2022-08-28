97a5ce21941991169903146f98f026f54256d1ec
Aryan Keluskar <ary.rpi6@gmail.com>
2022-08-28 08:13:41 +0530

    just the grid and verification

--- Puzzle.java
+++ Puzzle.java
@@ -13,14 +13,21 @@ public class Puzzle {
         super();
     }
 
-    // Generate a new puzzle given the number of cells to be guessed, which can be used
-    //  to control the difficulty level.
+    // Generate a new puzzle given the number of cells to be guessed, which can be
+    // used
+    // to control the difficulty level.
     // This method shall set (or update) the arrays numbers and isGiven
     public void newPuzzle(int cellsToGuess) {
         // I hardcode a puzzle here for illustration and testing.
         Genrate_Sudoku GN_Object = new Genrate_Sudoku(9, 10);
         int[][] hardcodedNumbers = GN_Object.returnSudoku();
-        int[][] hardcodedNumbers2 = hardcodedNumbers.clone();
+        int[][] hardcodedNumbers2 = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                hardcodedNumbers2[i][j] = hardcodedNumbers[i][j];
+            }
+        }
+
         // System.out.println(hardcodedNumbers.toString());
         Sudoku_Solver SS_Object = new Sudoku_Solver();
         SS_Object.solveSudoku(hardcodedNumbers, 9);
@@ -39,12 +46,12 @@ public class Puzzle {
         boolean[][] hardcodedIsGiven = new boolean[9][9];
         for (int row = 0; row < GameBoardPanel.GRID_SIZE; ++row) {
             for (int col = 0; col < GameBoardPanel.GRID_SIZE; ++col) {
-                hardcodedIsGiven[row][col]=hardcodedNumbers2[row][col]!=0;
-                System.out.print(hardcodedNumbers2[row][col]+" "+hardcodedIsGiven[row][col]);
+                hardcodedIsGiven[row][col] = hardcodedNumbers2[row][col] != 0;
+                System.out.print(hardcodedNumbers2[row][col] + " " + hardcodedIsGiven[row][col]);
             }
             System.out.println();
         }
-              
+
         // Copy from hardcodedIsGiven into array "isGiven"
         for (int row = 0; row < GameBoardPanel.GRID_SIZE; ++row) {
             for (int col = 0; col < GameBoardPanel.GRID_SIZE; ++col) {
